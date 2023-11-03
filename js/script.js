cafc3412d978337a6e6b97c610c70977954db391
MarouaneBRomdhane <133340397+MarouaneBRomdhane@users.noreply.github.com>
2023-11-03 14:29:08 +0100

    testing commands for DOM

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log(document);
+console.log("starting project to see if it is connected to js");
+document.title = "Updated TODO LIST";
+console.log(document);
+
+// get the input field  using the id
+var inputField = document.getElementById("taskName");
+console.log(inputField);
+console.dir(inputField); // this gives the methods and the properties of the inputField
